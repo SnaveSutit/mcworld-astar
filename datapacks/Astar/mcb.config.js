b06f722e291f157b8c888f52b9d11cf504977c55
SnaveSutit <snavesutit@gmail.com>
2025-11-06 15:43:23 -0500

    🐛 Fix some minor performance regressions, improve block weighting

--- datapacks/Astar/mcb.config.js
+++ datapacks/Astar/mcb.config.js
@@ -1,9 +1,31 @@
-module.exports = {
-	dev: false,
-	stepDebug: false,
-	header: '#built using mc-build (https://github.com/mc-build/mc-build)',
-	internalScoreboardName: 'astar.v',
-	sign(num) {
-		return num >= 0 ? `+${num}` : `${num}`
+const { readFileSync } = require('fs')
+
+let hotConfig
+
+const CONFIG = new Proxy(
+	{
+		header: '#built using mc-build (https://github.com/mc-build/mc-build)',
+		internalScoreboardName: 'astar.v',
+		sign(num) {
+			return num >= 0 ? `+${num}` : `${num}`
+		},
+		setup(mcb) {
+			mcb.events.onPreBuild.subscribe(() => {
+				hotConfig = JSON.parse(readFileSync('./hot.config.json', 'utf-8'))
+				Object.assign(mcb, hotConfig)
+			})
+		},
 	},
-}
+	{
+		get(target, prop) {
+			if (prop in target) {
+				return target[prop]
+			}
+			if (hotConfig && prop in hotConfig) {
+				return hotConfig[prop]
+			}
+		},
+	}
+)
+
+module.exports = CONFIG
