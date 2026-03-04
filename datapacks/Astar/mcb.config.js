cf665ea65b73b17dc449102b63fd428d9b22c07b
SnaveSutit <SnaveSutit@gmail.com>
2026-03-04 15:02:16 -0500

    🎨 Improved hot config system

--- datapacks/Astar/mcb.config.js
+++ datapacks/Astar/mcb.config.js
@@ -1,18 +1,36 @@
 const { readFileSync } = require('fs')
+const { default: chalk } = require('chalk')
+
+const env = process.argv.includes('--prod') ? 'prod' : 'dev'
+const HOT_CONFIG_PATH = `./config.${env}.json`
 
 let hotConfig
+
+const logPrefx = chalk.gray('[') + chalk.green('A*') + chalk.gray(']')
+
+function updateHotConfig() {
+	console.log(logPrefx, 'Using hot config from', chalk.cyan(HOT_CONFIG_PATH))
+	try {
+		const configText = readFileSync(HOT_CONFIG_PATH, 'utf-8')
+		hotConfig = JSON.parse(configText)
+	} catch (err) {
+		console.error(logPrefx, `Failed to read hot config from ${HOT_CONFIG_PATH}: ${err.message}`)
+		process.exit(1)
+	}
+}
 
 const CONFIG = new Proxy(
 	{
 		header: '#built using mc-build (https://github.com/mc-build/mc-build)',
 		internalScoreboardName: 'astar.v',
+		dontEmitComments: env === 'prod',
 		sign(num) {
 			return num >= 0 ? `+${num}` : `${num}`
 		},
 		setup(mcb) {
-			mcb.events.onPreBuild.subscribe(() => {
-				hotConfig = JSON.parse(readFileSync('./hot.config.json', 'utf-8'))
-				Object.assign(mcb, hotConfig)
+			mcb.events.onPreBuild.subscribe(build => {
+				console.log(logPrefx, `Building in`, chalk.red(env), 'mode')
+				updateHotConfig()
 			})
 		},
 	},
